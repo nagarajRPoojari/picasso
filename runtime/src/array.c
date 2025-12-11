9638527ff865c5728ba34cc571cb5bbe779e5f77
nagarajRPoojari <np137270@gmail.com>
2025-12-11 07:30:06 +0000

    update to use custotm allocator

--- runtime/src/array.c
+++ runtime/src/array.c
@@ -1,4 +1,5 @@
 #include "array.h"
+#include "alloc.h"
 #include <stdlib.h>
 /**
  * @brief allocate block of memory for array through GC_MALLOC 
@@ -6,9 +7,11 @@
  * @param count length of array
  * @param elem_size size of each element
  */
+extern __thread arena_t* __arena__;
+
 Array* lang_alloc_array(int count, int elem_size) {
-    Array* arr = (Array*)malloc(sizeof(Array));
+    Array* arr = (Array*)allocate(__arena__, sizeof(Array));
     arr->length = count;
-    arr->data = calloc(count, elem_size); /* should init with zero */
+    arr->data = allocate(__arena__, count*elem_size); /* should init with zero */
     return arr;
 }
