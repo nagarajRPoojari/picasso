35a53af41adb0306b86e5080d949d9f401869f7c
nagarajRPoojari <np137270@gmail.com>
2025-12-15 13:30:12 +0000

    refactor build & exec chain

--- CMakeFiles/always.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/always"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/always.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
