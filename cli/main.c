a3aabbd74c7b3af17ac54eda90ea136ddac3c022
nagarajRPoojari <np137270@gmail.com>
2025-12-15 14:38:57 +0000

    fix toolchain issue

---
+++ cli/main.c
@@ -0,0 +1,55 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void die(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "usage: niyama <build|exec> [file]\n");
+        return 1;
+    }
+
+    if (!strcmp(argv[1], "build")) {
+        if (argc != 3) {
+            fprintf(stderr, "niyama build <file.ini>\n");
+            return 1;
+        }
+
+        const char *file = argv[2];
+
+        if (system("mkdir -p .niyama") != 0) die("mkdir");
+        if (system("touch .niyama/out.ll") != 0) die("touch");
+
+
+        char cmd[1024];
+        snprintf(cmd, sizeof(cmd),
+            "bin/irgen gen %s .niyama/out.ll", file);
+        if (system(cmd) != 0) die("irgen");
+
+        if (system("llvm-as-16 .niyama/out.ll -o .niyama/out.bc") != 0)
+            die("llvm-as");
+
+        if (system("llc-16 -filetype=obj .niyama/out.bc -o .niyama/out.o") != 0)
+            die("llc");
+
+        if (system("cc .niyama/out.o ./bin/libruntime.a -o .niyama/a.out -luring -lpthread -lm") != 0)
+            die("link");
+
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "exec")) {
+        // Run the generated binary
+        execl("./.niyama/a.out", "./.niyama/a.out", NULL);
+        die("exec");
+    }
+
+    fprintf(stderr, "unknown command\n");
+    return 1;
+}
--- irgen/cmd/gen.go
+++ irgen/cmd/gen.go
@@ -11,31 +11,16 @@ import (
 // genCmd represents the gen command
 var genCmd = &cobra.Command{
 	Use:   "gen",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "generate IR",
+	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		c := generator.NewGenerator()
 		c.Build(args[0])
 		c.Compile()
-		c.Dump("output.ll")
+		c.Dump(args[1])
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(genCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// genCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// genCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
