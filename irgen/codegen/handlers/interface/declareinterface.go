868cd3131c63dfb2382643195ac685a7c8e6b182
nagarajRPoojari <np137270@gmail.com>
2025-12-21 14:05:15 +0000

    interface initial setup

---
+++ irgen/codegen/handlers/interface/declareinterface.go
@@ -0,0 +1,10 @@
+package interfaceh
+
+import (
+	"github.com/nagarajRPoojari/niyama/irgen/ast"
+	"github.com/nagarajRPoojari/niyama/irgen/codegen/handlers/state"
+)
+
+func (t *InterfaceHandler) DeclareInterface(cls ast.InterfaceDeclarationStatement, sourcePkg state.PackageEntry) {
+
+}
