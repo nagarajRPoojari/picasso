0d0c3f9cc8ed034e26fe1af11dc10aa406ca0fab
nagarajRPoojari <np137270@gmail.com>
2026-01-01 10:40:12 +0000

    fix: support additional methods for array lib

--- irgen/codegen/libs/array/array.go
+++ irgen/codegen/libs/array/array.go
@@ -21,6 +21,8 @@ func NewArrayHandler() *ArrayHandler {
 func (t *ArrayHandler) ListAllFuncs() map[string]function.Func {
 	funcs := make(map[string]function.Func)
 	funcs["create"] = t.create
+	funcs["len"] = t.len
+	funcs["shape"] = t.shape
 	return funcs
 }
 
@@ -33,4 +35,16 @@ func (t *ArrayHandler) create(th *tf.TypeHandler, module *ir.Module, bh *bc.Bloc
 		dims = append(dims, toInt)
 	}
 	return typedef.NewArray(bh, args[0].Type(), size.Load(bh), dims)
+}
+
+func (t *ArrayHandler) len(th *tf.TypeHandler, module *ir.Module, bh *bc.BlockHolder, args []typedef.Var) typedef.Var {
+	arr := args[0].(*tf.Array)
+	length := arr.Len(bh)
+	return length
+}
+
+func (t *ArrayHandler) shape(th *tf.TypeHandler, module *ir.Module, bh *bc.BlockHolder, args []typedef.Var) typedef.Var {
+	arr := args[0].(*tf.Array)
+	shape := arr.LoadShapeArray(bh)
+	return shape
 }
